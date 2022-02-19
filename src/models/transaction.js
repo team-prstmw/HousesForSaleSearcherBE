e85dde2295864dea91e818506bdef581f526e78e
mgoniszewska <marta.goniszewska@outlook.com>
2022-02-19 21:01:00 +0100

    Add ref to schema

--- src/models/transaction.js
+++ src/models/transaction.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const transactionSchema = new mongoose.Schema({
-    user: mongoose.Types.ObjectId,
-    owner: mongoose.Types.ObjectId,
-    house: mongoose.Types.ObjectId,
-    timestamp: {type: Date, default: Date.now},
-    price: Number
-  })
+const transactionSchema = Schema({
+  _id: Schema.Types.ObjectId,
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  owner: { type: Schema.Types.ObjectId, ref: 'User' },
+  house: { type: Schema.Types.ObjectId, ref: 'House' },
+  timestamp: { type: Date, default: Date.now },
+  price: Number,
+});
 
 const Transaction = mongoose.model('Transaction', transactionSchema);
 
