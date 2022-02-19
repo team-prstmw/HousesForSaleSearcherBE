a3d12187d7c31e1a498a013a47095851e067f860
mgoniszewska <marta.goniszewska@outlook.com>
2022-02-19 16:08:58 +0100

    Move route to controllers, adjust code

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import helloControllers from './hello.controllers';
+import transactionControllers from './transactions.controllers'
 
 const router = Router();
 
 helloControllers(router);
+transactionControllers(router);
 
 export default router;
---
+++ src/controllers/transactions.controllers.js
@@ -0,0 +1,21 @@
+import { StatusCodes } from 'http-status-codes';
+
+const transactionControllers = (router) => {
+  router.post('/transactions', async (req, res) => {
+    const body = req.body; // {userId: 'buyer', houseId: '', price: 10000}
+
+    const response = await transactionService.buyHouse(body);
+
+    if (!response?.status) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status: 'server error' });
+    }
+
+    if (response.status === 'invalid') {
+      return res.status(StatusCodes.BAD_REQUEST).json(response);
+    }
+
+    return res.status(StatusCodes.CREATED).json(response);
+  });
+};
+
+return transactionControllers;
