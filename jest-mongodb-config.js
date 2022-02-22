2c1d8f8a8aa0b7cf137b1cf9723967090b84e5b4
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-02-22 19:41:15 +0100

    configuration of jest

---
+++ jest-mongodb-config.js
@@ -0,0 +1,14 @@
+// Same DB for all tests - pass the config like this:
+
+module.exports = {
+  mongodbMemoryServerOptions: {
+    binary: {
+      version: '4.0.3',
+      skipMD5: true,
+    },
+    instance: {
+      dbName: 'jest',
+    },
+    autoStart: false,
+  },
+};
