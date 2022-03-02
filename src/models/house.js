009558bf200e2a01e59c07ba01b63feacc30c803
Wojciech Okularczyk <wojciech.okularczyk@coderscrew.pl>
2022-03-02 19:53:28 +0100

    prettier

--- src/models/house.js
+++ src/models/house.js
@@ -1,104 +1,105 @@
-const mongoose = require('mongoose')
-const { Schema } = mongoose
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const HouseSchema = new Schema({
+const HouseSchema = new Schema(
+  {
     houseId: {
-        type: Schema.Types.ObjectId,
-        default: new mongoose.Types.ObjectId
+      type: Schema.Types.ObjectId,
+      default: new mongoose.Types.ObjectId(),
     },
     owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
     },
-    address:{
-        country:{
-            type: String,
-            required: true
-        },
-        region:{
-            type: String
-        },
-        city:{
-            type: String,
-            required: true
-        },
-        district: {
-            type:String,
-        },
-        street:{
-            type: String,
-            required: true
-        },
-        houseNr: {
-            type: String,
-            required: true
-        },
-        flatNr: {
-            type: String,
-            default: ''
-        }
+    address: {
+      country: {
+        type: String,
+        required: true,
+      },
+      region: {
+        type: String,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      district: {
+        type: String,
+      },
+      street: {
+        type: String,
+        required: true,
+      },
+      houseNr: {
+        type: String,
+        required: true,
+      },
+      flatNr: {
+        type: String,
+        default: '',
+      },
     },
     houseType: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    attributes:{
-        nrOfRooms:{
-            type: Number,
-            required: true
-        },
-        nrOfBathrooms:{
-            type: Number,
-            required: true
-        },
-        floors:{
-            type: Number,
-            required: true
-        },
-        heating:{
-            type: String,
-            required: true,
-            lowercase: true
-        }
-    },
-    otherFeatures:{
-        type: Array,
-        default: []
-    },
-    constructionDate:{
+    attributes: {
+      nrOfRooms: {
         type: Number,
+        required: true,
+      },
+      nrOfBathrooms: {
+        type: Number,
+        required: true,
+      },
+      floors: {
+        type: Number,
+        required: true,
+      },
+      heating: {
+        type: String,
+        required: true,
+        lowercase: true,
+      },
+    },
+    otherFeatures: {
+      type: Array,
+      default: [],
+    },
+    constructionDate: {
+      type: Number,
     },
     area: {
-        type: Number,
-        require: true
+      type: Number,
+      require: true,
     },
     price: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     meta: {
-        viewsCounter: {
-            type: Number,
-            default: 0
+      viewsCounter: {
+        type: Number,
+        default: 0,
+      },
+      favoritesCounter: {
+        type: Number,
+        default: 0,
+      },
+      location: {
+        lat: {
+          type: Number,
+          required: true,
         },
-        favoritesCounter: {
-            type: Number,
-            default: 0
+        lng: {
+          type: Number,
+          required: true,
         },
-        location: {
-            lat:{
-                type: Number,
-                required: true
-            },
-            lng:{
-                type: Number,
-                required: true
-            }
-        },
-    }
-},
-{timestamps: true}
-)
+      },
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Houses', HouseSchema)
+module.exports = mongoose.model('Houses', HouseSchema);
--- src/services/findAddress.js
+++ src/services/findAddress.js
@@ -1,13 +1,13 @@
-import createNewHouse from '../models/house'
+import createNewHouse from '../models/house';
 
 export const findAddress = async (addressData) => {
-    const {country, city, street, houseNr, flatNr} = {...addressData}
-    const address = await createNewHouse.findOne({
-      'address.country': country,
-      'address.city': city,
-      'address.street': street,
-      'address.houseNr': houseNr,
-      'address.flatNr': flatNr
-    })
-    if (address) throw 'House with a given address already exists'};
-
+  const { country, city, street, houseNr, flatNr } = { ...addressData };
+  const address = await createNewHouse.findOne({
+    'address.country': country,
+    'address.city': city,
+    'address.street': street,
+    'address.houseNr': houseNr,
+    'address.flatNr': flatNr,
+  });
+  if (address) throw 'House with a given address already exists';
+};
