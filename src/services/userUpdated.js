4180c9d1e75af68812270e1a27f7ad2e5e1a5727
unknown <jurczykpatryk8@gmail.com>
2022-03-07 21:00:53 +0100

    Changes after CR

--- src/services/userUpdated.js
+++ src/services/userUpdated.js
@@ -1,17 +1,20 @@
-import User from "../models/user";
+import User from '../models/user';
 
 const userUpdated = async (req) => {
+  try {
     const user = await User.findOneAndUpdate(
-        {
-          _id: req.params.id,
-        },
-        req.body,
-        { new: true }
+      {
+        _id: req.params.id,
+      },
+      req.body,
+      { new: true }
     );
-    
-    if (!user) return { status: 'invalid', message: 'User not found' };
 
+    if (!user) return { status: 'invalid', message: 'User not found' };
     return { message: 'Updated' };
-}
+  } catch {
+    return { status: 'invalid', message: 'User not found' };
+  }
+};
 
 export default userUpdated;
