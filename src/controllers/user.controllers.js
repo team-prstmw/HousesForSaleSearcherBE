1d0db060e77ca17e61b42dca39353c26f3cde6c7
unknown <jurczykpatryk8@gmail.com>
2022-03-13 21:03:59 +0100

    fix

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -37,9 +37,8 @@ export const userLogin = async (data) => {
   if (error) return { status: 'invalid', message: error.details[0].message };
 
   const activeUser = await User.find({ email: data.email, status: { $eq: USER_ACTIVE } });
-  if (!activeUser[0] || activeUser[0].status === USER_DELETED) {
+  if (!activeUser[0] || activeUser[0].status === USER_DELETED)
     return { status: 'invalid', message: 'Email or password is wrong' };
-  }
 
   const validPass = await bcrypt.compare(data.password, activeUser[0].password);
   if (!validPass) return { status: 'invalid', message: 'Email or password is wrong' };
